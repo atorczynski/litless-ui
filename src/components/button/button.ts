aed5da5eb6122c65aec72e9ff93b1995dfa70833
atoczynski <artur.torcz@gmail.com>
2024-12-15 18:30:35 +0100

    Add button component and update dependencies in package.json; enhance tsconfig for better output

---
+++ src/components/button/button.ts
@@ -0,0 +1,10 @@
+import { html } from "lit";
+import { LitElement } from "lit";
+import { customElement } from "lit/decorators.js";
+
+@customElement("button")
+export class Button extends LitElement {
+  render() {
+    return html`<button><slot></slot></button>`;
+  }
+}
